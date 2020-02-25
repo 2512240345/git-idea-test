18182372d2533e2e04246a72df6260cf45f4b4e4
e <l>
2020-02-25 22:26:08 +0800

    第六次修改，向github发

--- src/test/java/com/baizhi/GitIdeaTestApplicationTests.java
+++ src/test/java/com/baizhi/GitIdeaTestApplicationTests.java
@@ -19,6 +19,8 @@ public class GitIdeaTestApplicationTests {
         }
         System.out.println("分支");
         System.out.println("第五次修改");
+        int n = 1;
+        System.out.println(n);
     }
 
 }
